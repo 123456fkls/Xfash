4a02f95d90b02da94e700b0123a0bd3b847635d3
123456fkls <123456fkls@gmail.com>
2026-01-08 16:58:06 +0800

    补充了员工管理的PageHelper插件来方便分页管理

--- demo/src/main/java/com/example/xfash/Mapper/EmpMapper.java
+++ demo/src/main/java/com/example/xfash/Mapper/EmpMapper.java
@@ -9,12 +9,16 @@ import java.util.List;
 
 @Mapper
 public interface EmpMapper {
+//----------------------------------原始分页 ----------------------------------
 //    查询所有员工信息记录数
-    @Select("select count(*) from emp left join dept on emp.dept_id = dept.id")
-    public Long count();
-//    查询所有员工信息(分页)
+//    @Select("select count(*) from emp left join dept on emp.dept_id = dept.id")
+//    public Long count();
+    //查询所有员工信息(分页)
+//    @Select("select emp.*,dept.name deptName from emp left join dept on emp.dept_id = dept.id " +
+//            "order by emp.update_time desc " +
+//            "limit #{start},#{pageSize}")
+//    public List<Emp> list(Integer start,Integer pageSize);
     @Select("select emp.*,dept.name deptName from emp left join dept on emp.dept_id = dept.id " +
-            "order by emp.update_time desc " +
-            "limit #{start},#{pageSize}")
-    public List<Emp> list(Integer start,Integer pageSize);
+            "order by emp.update_time desc ")
+    public List<Emp> list();
 }
--- demo/src/main/java/com/example/xfash/Service/Impl/EmpServiceImpl.java
+++ demo/src/main/java/com/example/xfash/Service/Impl/EmpServiceImpl.java
@@ -4,6 +4,8 @@ import com.example.xfash.Mapper.EmpMapper;
 import com.example.xfash.Service.EmpService;
 import com.example.xfash.pojo.Emp;
 import com.example.xfash.pojo.PageResult;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,15 +16,27 @@ import java.util.List;
 public class EmpServiceImpl implements EmpService {
     @Autowired
     private EmpMapper empMapper;
-    @Override
-    public PageResult<Emp> page(Integer page, Integer pageSize) {
-        //调用mapper接口，查询总记录数
-         Long total =empMapper.count();
-        //调用mapper接口，查询结果列表
-        List<Emp> rows = empMapper.list((page-1)*pageSize,pageSize);
-        //组装PageResult对象并返回
-        return new PageResult<Emp>(total,rows);
+//    //原始分页查询操作
+//    @Override
+//    public PageResult<Emp> page(Integer page, Integer pageSize) {
+//        //调用mapper接口，查询总记录数
+//         Long total =empMapper.count();
+//        //调用mapper接口，查询结果列表
+//        List<Emp> rows = empMapper.list((page-1)*pageSize,pageSize);
+//        //组装PageResult对象并返回
+//        return new PageResult<Emp>(total,rows);
+  //  }
+    //PageHelper分页查询
+public PageResult<Emp> page(Integer page, Integer pageSize) {
+    //设置分页参数
+    PageHelper.startPage(page,pageSize);
+    //执行查询
+    List<Emp> empList = empMapper.list();
+    //解析结果并封装数据
+    Page<Emp> p = (Page<Emp>) empList;
+    return new PageResult<Emp>(p.getTotal(),p.getResult());
 
 
-    }
+
+      }
 }
